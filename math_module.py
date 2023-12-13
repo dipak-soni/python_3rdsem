372779ce61a59fa6b28451b30e0a466515933111
dipak-soni <152852933+dipak-soni@users.noreply.github.com>
2023-12-13 17:07:12 +0530

    Update math_module.py

--- math_module.py
+++ math_module.py
@@ -5,7 +5,7 @@ c=12.4
 print("1. square root of the number is:",math.sqrt(a))
 print("2. Greatest common divisior is:",math.gcd(a,b))
 print("3. lowest common multiplier is:",math.lcm(a,b))
-print("4. floor and ceiling value of a number is:%d and %d"%(math.floor(),math.ceil()))
+print("4. floor and ceiling value of a number is:%d and %d"%(math.floor(c),math.ceil(c)))
 print("5. factorial of the given number is:",math.factorial(a))
 print("6. exponential of the number is",math.exp(a))
 print("6.1 exponential of the number is:",math.e**a)
