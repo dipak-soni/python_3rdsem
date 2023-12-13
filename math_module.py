5237992e919e9d8cc812bedbb750bc2269c468e5
dipak-soni <152852933+dipak-soni@users.noreply.github.com>
2023-12-13 10:50:10 +0530

    Create math_module.py

---
+++ math_module.py
@@ -0,0 +1,13 @@
+import math
+a=12
+b=15
+c=12.4
+print("1. square root of the number is:",math.sqrt(a))
+print("2. Greatest common divisior is:",math.gcd(a,b))
+print("3. lowest common multiplier is:",math.lcm(a,b))
+print("4. floor and ceiling value of a number is:%d and %d"%(math.floor,math.ceiling))
+print("5. factorial of the given number is:",math.factorial(a))
+print("6. exponential of the number is",math.exp(a))
+print("6.1 exponential of the number is:",math.e**a)
+print("7. power 3 of a number is:",pow(a,3))
+print("7.1 power 3 of a number is:",a**3)
