19377c03ed1933a385fadfd00863c2950660a6e9
dipak-soni <152852933+dipak-soni@users.noreply.github.com>
2023-12-13 17:27:42 +0530

    Additional code

--- GCD.py
+++ GCD.py
@@ -1,1 +1,6 @@
-print("Hello")
+def gcd(a,b):
+        for y in range(a if a>=b else b+1,0,-1):
+            if a%y==0 and b%y==0:
+                print("GCD is:",y)
+                break
+gcd(int(input("Enter first number:")),int(input("Enter secon
