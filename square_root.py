447d9eb20bce4d3c4b64d1be99b49c426ff43c44
dipak-soni <152852933+dipak-soni@users.noreply.github.com>
2023-12-13 19:38:53 +0530

    commit

---
+++ square_root.py
@@ -0,0 +1,29 @@
+def find_low_high(number):
+    low=high=0
+    for x in range(1,number,1):
+        if x*x<number:
+            low=x
+    high=low+1
+    return [low,high]
+
+def square_root(number):
+     if number<0:
+         raise "Enter only positive number"
+     x=find_low_high(number) if number>1 else [0,1]
+     low=x[0]
+     high=x[1]
+     while low<=high:
+        mid=(low+high)/2
+        if mid*mid==number:
+           print(mid)
+           break
+        elif mid*mid<number:
+           low=mid+0.0001
+        else:
+           high=mid-0.0001  
+     else:
+        print("square root is:%.2f"%mid)
+                    
+         
+     
+square_root(-0.04)
