04dd8aee7853a06863638e449f542d311ea431cc
dipak-soni <152852933+dipak-soni@users.noreply.github.com>
2023-12-15 21:37:57 +0530

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -1,5 +1,5 @@
 def gcd(a,b):
-        for y in range(a if a>=b else b+1,0,-1):
+        for y in range(a if a<=b else b,0,-1):
             if a%y==0 and b%y==0:
                 print("GCD is:",y)
                 break
